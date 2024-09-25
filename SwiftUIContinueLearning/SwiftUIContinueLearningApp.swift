b7a639e06d44e9f0a02dcfca571c79d96ba1befc
muhmmad-zeeshan <rafaq657@gmail.com>
2024-09-26 01:27:47 +0500

    Initial Commit

---
+++ SwiftUIContinueLearning/SwiftUIContinueLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIContinueLearningApp.swift
+//  SwiftUIContinueLearning
+//
+//  Created by Muhammad Zeeshan on 26/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIContinueLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
